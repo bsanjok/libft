a8b0c0271685a18d9193b7548b62ef46064cc0f0
sanjok <bsanjok@live.com>
2024-12-14 22:44:44 +0100

    using del function when memory allocation fails with ft_lstnew function

--- src/libft_ft/libft_list/ft_lstmap.c
+++ src/libft_ft/libft_list/ft_lstmap.c
@@ -18,16 +18,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*newlist;
 
 	newlist = NULL;
-	del = NULL;
 	if (!lst || !f)
 		return (NULL);
-	while (lst->next)
+	while (lst)
 	{
 		newelem = ft_lstnew(f(lst->content));
+		if (newelem == NULL)
+		{
+			ft_lstclear(&newelem, del);
+			return (NULL);
+		}
 		ft_lstadd_back(&newlist, newelem);
 		lst = lst->next;
 	}
-	newelem = ft_lstnew(f(lst->content));
 	ft_lstadd_back(&newlist, newelem);
 	return (newlist);
 }
